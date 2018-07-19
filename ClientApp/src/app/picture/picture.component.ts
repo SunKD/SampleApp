f4b344c41f95954fef09db3022c2cfb037976fe9
Sun Davey <sundavey@hotmail.com>
2018-07-19 15:23:39 -0700

    added picture toggle function

--- ClientApp/src/app/picture/picture.component.ts
+++ ClientApp/src/app/picture/picture.component.ts
@@ -10,13 +10,16 @@ export class PictureComponent implements OnInit {
   msg = "Click the submit button to reveal the picture."
   InitialClick = false;
 
+  beach: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmit(){
+  onSubmit() {
     this.InitialClick = true;
+    this.beach = !this.beach;
+    console.log("change it!!")
   }
 
 }
